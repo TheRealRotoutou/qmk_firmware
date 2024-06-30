10d49a748cc1606bd47019a246c686075aebee19
TheRealRotoutou <rotoutou@gmail.com>
2024-06-30 19:00:37 +0200

    init with first version

---
+++ keyboards/crkbd/keymaps/TheRealRotoutou/README.md
@@ -0,0 +1,6 @@
+# Rotoutou's layout for Corne
+
+Features:
+* Azerty based - Use with locale "Français (France)"
+* Accents are available with combos : hit Z and E simultanely for é for example
+* For basic corne : No sound / No oled / No RGB lights under keys or keyboard
---
+++ keyboards/crkbd/keymaps/TheRealRotoutou/keymap.c
@@ -0,0 +1,108 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "keymap_french.h"
+
+enum layers {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _NUMBER,
+  _SIGNS
+};
+
+const uint16_t PROGMEM FR_EGRV_combo[] = {FR_Z, FR_E, COMBO_END};
+const uint16_t PROGMEM FR_EACU_combo[] = {FR_E, FR_R, COMBO_END};
+const uint16_t PROGMEM FR_UGRV_combo[] = {FR_Y, FR_U, COMBO_END};
+const uint16_t PROGMEM FR_AGRV_combo[] = {FR_A, FR_Z, COMBO_END};
+const uint16_t PROGMEM FR_CCED_combo[] = {FR_X, FR_C, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(FR_EGRV_combo, FR_EGRV),
+    COMBO(FR_EACU_combo, FR_EACU),
+    COMBO(FR_UGRV_combo, FR_UGRV),
+    COMBO(FR_AGRV_combo, FR_AGRV),
+    COMBO(FR_CCED_combo, FR_CCED)
+};
+
+// Base layer - Middle of the key - black
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------------------.                          ,-----------------------------------------------------------------------.
+          KC_TAB,       FR_A,       FR_Z,       FR_E,       FR_R,       FR_T,                                  FR_Y,       FR_U,       FR_I,       FR_O,       FR_P,    KC_BSPC,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         KC_LSFT,       FR_Q,       FR_S,       FR_D, LT(3,FR_F),       FR_G,                                  FR_H, LT(4,FR_J),       FR_K,       FR_L,       FR_M,     KC_DEL,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         KC_LCTL,       FR_W,       FR_X,       FR_C,       FR_V,       FR_B,                                  FR_N,    FR_COMM,     FR_DOT,    FR_COLN,    FR_QUES,     KC_ESC,
+  //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
+                                                         KC_LALT,      MO(1),     KC_SPC,        KC_ENT,      MO(2),     KC_RALT
+                                                      //`-------------------------------'  `-----------------------------------'
+  ),
+
+// Lower layer - Lower left in the key - blue
+    [_LOWER] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------------------.                          ,-----------------------------------------------------------------------.
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                               FR_UNDS,     FR_EQL,    FR_EXLM,    FR_SECT,    FR_TILD,      FR_AT,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, LCTL(FR_X),     KC_DEL,                               KC_LEFT,    KC_DOWN,      KC_UP,    KC_RGHT,    KC_HOME,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, LCTL(FR_C), LCTL(FR_V),                               XXXXXXX,    KC_PGDN,    KC_PGUP,    XXXXXXX,     KC_END,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
+                                                         _______,    _______,    _______,        _______,   _______,     _______
+                                                      //`-------------------------------'  `-----------------------------------'
+  ),
+
+// Raise layer - Lower right  in the key - green
+    [_RAISE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------------------.                          ,-----------------------------------------------------------------------.
+         XXXXXXX,    XXXXXXX,     FR_GRV,    FR_QUOT,    FR_CIRC,    FR_DIAE,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+           KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+           KC_F7,      KC_F8,      KC_F9,     KC_F10,     KC_F11,     KC_F12,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
+                                                         _______,    _______,    _______,       _______,    _______,     _______
+                                                      //`-------------------------------'  `-----------------------------------'
+  ),
+
+// Number layer - Upper left in the key - red
+    [_NUMBER] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------------------.                          ,-----------------------------------------------------------------------.
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                  FR_7,       FR_8,       FR_9,    FR_SLSH,    FR_MINS,    FR_EURO,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                  FR_4,       FR_5,       FR_6,    FR_ASTR,    FR_PLUS,     FR_DLR,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                  FR_1,       FR_2,       FR_3,    FR_HASH,    FR_PERC,     FR_DEG,
+  //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
+                                                         _______,    _______,    _______,       _______,       FR_0,      FR_DOT
+                                                      //`-------------------------------'  `-----------------------------------'
+  ),
+
+// Signs layer - Upper right in the key - orange
+    [_SIGNS] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------------------------.                          ,-----------------------------------------------------------------------.
+         FR_SUP2,    FR_SCLN,    FR_DQUO,    FR_QUOT,    FR_LPRN,    FR_RPRN,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         FR_MICR,    FR_AMPR,    FR_LABK,    FR_RABK,    FR_LBRC,    FR_RBRC,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------|                          |-----------+-----------+-----------+-----------+-----------+-----------|
+         FR_CURR,    FR_PIPE,    FR_BSLS,    FR_SLSH,    FR_LCBR,    FR_RCBR,                               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+  //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
+                                                         _______,    _______,    _______,       _______,    _______,     _______
+                                                      //`-------------------------------'  `-----------------------------------'
+ )
+};
